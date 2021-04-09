3824fae78b77d503408d4916644b673f96268cab
Lucas Teixeira <lucas.vieira@brmalls.com.br>
2021-04-09 12:00:15 -0300

    - feat: criando camada de serviço de produto; - feat: criando exception para recurso não encontrado.

--- crud/src/main/java/com/lucasvieira/crud/service/ProdutoService.java
+++ crud/src/main/java/com/lucasvieira/crud/service/ProdutoService.java
@@ -1,9 +1,15 @@
 package com.lucasvieira.crud.service;
 
 import com.lucasvieira.crud.data.vo.ProdutoVO;
+import com.lucasvieira.crud.entity.Produto;
+import com.lucasvieira.crud.exceptions.ResourceNotFoundException;
 import com.lucasvieira.crud.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -16,6 +22,38 @@ public class ProdutoService {
     }
 
     public ProdutoVO create(ProdutoVO produtoVO) {
-        return null;
+        ProdutoVO proVoRetorno = ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+        return proVoRetorno;
+    }
+
+    public Page<ProdutoVO> findAll(Pageable pageable) {
+        var page = produtoRepository.findAll(pageable);
+        return page.map(this::convertToProdutoVO);
+    }
+
+    private ProdutoVO convertToProdutoVO(Produto produto) {
+        return ProdutoVO.create(produto);
+    }
+
+    public ProdutoVO findById(Long id) {
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado."));
+        return ProdutoVO.create(entity);
+    }
+
+    public ProdutoVO update(ProdutoVO produtoVO) {
+        final Optional<Produto> optionalProduto = produtoRepository.findById(produtoVO.getId());
+
+        if(!optionalProduto.isPresent()) {
+            throw new ResourceNotFoundException("Recurso não encontrado.");
+        }
+
+        return ProdutoVO.create(produtoRepository.save(Produto.create(produtoVO)));
+    }
+
+    public void delete(Long id) {
+        var entity = produtoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado."));
+        produtoRepository.delete(entity);
     }
 }
